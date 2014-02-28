02f8462c7d2c558767d5fa33069cb7b72257b11f
Stephen Babineau <tien_ralter@hotmail.com>
2014-02-28 10:24:39 -0800

    Added wsgiref and application level function

--- get_stocks.py
+++ get_stocks.py
@@ -26,14 +26,38 @@ def btc_last():
 def combine():
     symbol, close, time = get_quote('tsla')
     bitcoin = btc_last()
-    return "%s\n%s: %.6f BTC" % (
+    return "%s\r\n%s: %.6f BTC" % (
         time.encode('utf-8'),
         symbol.encode('utf-8'),
         (close / bitcoin)
     )
 
 
+def application(environ, start_response):
+    headers = [("Content-type", "text/html")]
+    # body = combine()
+    # headers.append(('Content-length', str(len(body))))
+    # start_response('200 OK', headers)
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        body = combine()
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body]
+    return [body]
+
+
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    srv = make_server('localhost', 8000, combine)
+    srv = make_server('localhost', 8000, application)
     srv.serve_forever()
